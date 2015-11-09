936d1f10b1b341db1c27f37c4c2fe49c56bfe1da
Hisham <hishamdiu@gmail.com>
2015-11-09 13:24:34 +0600

    fff

--- CorporateAPIAutomation/src/test/java/authentication/LoginAPI2Test.java
+++ CorporateAPIAutomation/src/test/java/authentication/LoginAPI2Test.java
@@ -11,7 +11,6 @@ import java.util.Map;
 
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.equalTo;
 
 
 public class LoginAPI2Test {
@@ -38,62 +37,9 @@ public class LoginAPI2Test {
 				.when()
 					.post(config.get("URL"))
 				.then()
-					.statusCode(200)
-					//.body(matchesJsonSchemaInClasspath(config.get("SchemaPath")))
-					.body(matchesJsonSchemaInClasspath("jsonSchema/login_pass.json"))
-					.body("Params.UserName", equalTo("anik"))
-					.body("Params.Password", equalTo("12345678"))
-					.body("Data.UserName", equalTo("anik"))
-					.body("Data.LastName", equalTo("Hoque"))
-					.body("Data.FirstName", equalTo("Mushraful"))
-					.extract().response();
-
-		
-		/*System.out.println(responseJSON.getCookies());
-		System.out.println(responseJSON.getSessionId());
-		System.out.println(responseJSON.getStatusLine());
-		System.out.println(responseJSON.getBody().asString());*/
-
-
-/*
-		if (responseJSON.getStatusCode() == 200) {
-			if ( responseJSON.jsonPath().getList("Reasons") == null ) {
-				Assert.assertEquals(responseJSON.jsonPath().getString("Params.UserName"),
-						"anik", "UserName not found on Response Params");
-				Assert.assertEquals(responseJSON.jsonPath().getString("Params.Password"),
-						"12345678", "UserName not found on Response Params");
-
-				Assert.assertEquals(responseJSON.jsonPath().getString("Data.UserName"),
-						"anik", "UserName not found on Response Data");
-				Assert.assertNotNull(responseJSON.jsonPath().getString("Data.LastName"));
-				Assert.assertNotNull(responseJSON.jsonPath().getString("Data.FirstName"));
-			} else {
-				System.out.println(responseJSON.jsonPath().getList("Reasons"));
-				Assert.assertNotNull(responseJSON.jsonPath().getList("Reasons"), "Reasons field is null");
-				Assert.assertNull(responseJSON.jsonPath().getString("Data"), "Response Data is not null");	
-			}
-
-
-
-			//	Assert.assertNotNull(responseJSON.jsonPath().getString("Data.LastName"));
-			//	Assert.assertNotNull(responseJSON.jsonPath().getString("Data.FirstName"));
-
-			*/
-/*	} else {
-
-			Assert.assertEquals(responseJSON.jsonPath().getString("Params.UserName"),
-					"anik", "UserName not found on Response Params");
-			Assert.assertNull(responseJSON.jsonPath().getString("Data"), "Response Data is not null");
-			Assert.assertNotNull(responseJSON.jsonPath().getString("Reasons.ReasonCode"));
-		}*//*
-
-
-		}
-*/
-
-		System.out.println("**********Response from [Login]**********");
-		System.out.println(responseJSON.getBody().asString());
-		System.out.println();
+						.statusCode(200)
+						.body(matchesJsonSchemaInClasspath(config.get("SchemaPath")))
+						.extract().response();
 
 	}
 

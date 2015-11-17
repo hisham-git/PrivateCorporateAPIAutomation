0b1d73c9d8c08196908131179c83cfa7b6089d4f
sazzad <sazzad@dsinnovators.com>
2015-11-17 16:42:12 +0600

    corporate user insert done update added

---
+++ CorporateAPIAutomation/src/test/java/api/corporate/UserServiceTest.java
@@ -0,0 +1,27 @@
+package api.corporate;
+
+import org.json.JSONException;
+import org.testng.annotations.Test;
+import utilities.APICaller;
+import utilities.dataProvider.ExcelFileReaderConfig;
+
+import java.util.Date;
+import java.util.Map;
+
+
+public class UserServiceTest {
+
+    @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
+    public void insertUser(Map<String, String> config) throws JSONException {
+        String body="{\"Header\":{},\"Params\":{\"UserName\":\"sazzad"+(new Date().getTime())+"@gmail.com\",\"Password\":\"12345678\",\"FirstName\":\"abc\",\"LastName\":\"bcd\",\"EmailAddress\":\"sazzad"+(new Date().getTime())+"@gmail.com\"}}";
+        APICaller.callApi(config.get("URL"), body, config.get("SchemaPath"));
+    }
+
+    @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
+    public void updateUser(Map<String, String> config) throws JSONException {
+        //APICaller.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"));
+        //api error do later
+    }
+
+
+}

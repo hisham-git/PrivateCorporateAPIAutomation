a753e89f6622d83c1a5f4afd6afc7f0f308fc303
sazzad <sazzad@dsinnovators.com>
2015-11-17 18:35:45 +0600

    authorize setup few done

--- CorporateAPIAutomation/src/test/java/api/authorize/SetupServiceTest.java
+++ CorporateAPIAutomation/src/test/java/api/authorize/SetupServiceTest.java
@@ -5,28 +5,64 @@ import org.testng.annotations.Test;
 import utilities.APICaller;
 import utilities.dataProvider.ExcelFileReaderConfig;
 
+import java.util.Date;
 import java.util.Map;
 
+/**
+ *  deleteRole check id in db and xl sheet before calling
+ */
 public class SetupServiceTest {
 
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
-    public void testInsertRoleAPI(Map<String, String> config) throws JSONException {
+    public void getLogicalRoles(Map<String, String> config) throws JSONException {
         APICaller.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"));
     }
 
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
-    public void testUpdateRoleAPI(Map<String, String> config) throws JSONException {
+    public void insertRole(Map<String, String> config) throws JSONException {
+        String body = "{\"Header\":{},\"Params\":{\"Name\":\"Role"+(new Date().getTime())+"\",\"Description\":\"Test Description\"}}";
+        APICaller.callApi(config.get("URL"), body, config.get("SchemaPath"));
+    }
+
+    @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
+    public void updateRole(Map<String, String> config) throws JSONException {
         APICaller.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"));
     }
 
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
-    public void testDeleteRoleAPI(Map<String, String> config) throws JSONException {
-        APICaller.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"));
-    }
-
-    @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
-    public void testGetRightsAPI(Map<String, String> config) throws JSONException {
+    public void deleteRole(Map<String, String> config) throws JSONException {
         APICaller.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"));
     }
 
 }
+
+/*
+
+
+insertRole
+updateRole
+deleteRole
+
+
+
+
+authorize	setup	getRoles
+        authorize	setup	getLogicalRoles
+        authorize	setup	insertRole
+        authorize	setup	updateRole
+        authorize	setup	deleteRole
+        authorize	setup	getRights
+        authorize	setup	getRoleUsers
+        authorize	setup	getUserRoles
+        authorize	setup	getUsers
+        authorize	setup	addRightsToRole
+        authorize	setup	removeRightsFromRole
+        authorize	setup	addUsersToRole
+        authorize	setup	removeUsersFromRole
+        authorize	setup	insertUserStatus
+        authorize	setup	updateUserStatus
+        authorize	setup	deleteUserStatus
+        authorize	setup	getUserStatus
+        authorize	setup	getStatusRequestUsers
+        authorize	setup	addStatusRequest
+        authorize	setup	removeStatusRequest*/

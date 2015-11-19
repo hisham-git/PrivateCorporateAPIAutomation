bae168aa016ea48b2fcdeb222b2c2014f122c723
sazzad <sazzad@dsinnovators.com>
2015-11-19 16:52:23 +0600

    corporate user all senario done error in insert update

--- CorporateAPIAutomation/src/test/java/api/corporate/UserServiceTest.java
+++ CorporateAPIAutomation/src/test/java/api/corporate/UserServiceTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import utilities.Util;
 import utilities.dataProvider.ExcelFileReaderConfig;
 
-import java.util.Date;
 import java.util.Map;
 
 
@@ -13,19 +12,10 @@ public class UserServiceTest {
 
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
     public void insertUser(Map<String, String> config) throws JSONException {
-        String body="{\"Header\":{},\"Params\":{\"UserName\":\"sazzad"+(new Date().getTime())+"@gmail.com\"," +
-                "\"Password\":\"12345678\",\"FirstName\":\"abc\",\"LastName\":\"bcd\"" +
-                ",\"EmailAddress\":\"sazzad"+(new Date().getTime())+"@gmail.com\"}}";
-        String body2="{\"Header\":{},\"Params\":{\"UserName\":\"sazzad"+(new Date().getTime())+"@gmail.com\"," +
-                "\"Password\":\"12345678\",\"FirstName\":\"abc\",\"Lastame\":\"bcd\"" +
-                ",\"EmailAddress\":\"sazzad"+(new Date().getTime())+"@gmail.com\"}}";
-        String body3="{\"Header\":{},\"Params\":{\"UserName\":\"sazzad"+(new Date().getTime())+"@gmail.com\"," +
-                "\"Password\":\"12345678\",\"FirstName\":\"abc\",\"LastName\":\"bcd\"" +
-                ",\"EmailAddress\":\"sazzad"+(new Date().getTime())+"@gmail.com\",\"oca\":2}}";
-        Util.callApi(config.get("URL"), body, config.get("SchemaPath"), null);
+        Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
     }
 
-    @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
+    //@Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
     public void updateUser(Map<String, String> config) throws JSONException {
         //Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"));
         //api error do later

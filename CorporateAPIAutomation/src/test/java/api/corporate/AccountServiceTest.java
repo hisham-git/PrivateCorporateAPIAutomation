7d26a5a1eee72192799f65a4ed3331e00cba7357
Hisham <hishamdiu@gmail.com>
2015-12-15 20:06:24 +0600

    Refactoring Account Service, Adding Test Case Description in Log...

--- CorporateAPIAutomation/src/test/java/api/corporate/AccountServiceTest.java
+++ CorporateAPIAutomation/src/test/java/api/corporate/AccountServiceTest.java
@@ -16,35 +16,27 @@ import java.util.Map;
 public class AccountServiceTest {
 
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
-    public void getAccounts(Map<String, String> config) throws JSONException {
-    	
-    	Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
-    	
-    	/*try {
-    		Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
-    		System.out.println( config.get("Test Case") + " => Passed"  );
-		} catch (AssertionError e) {
-			System.out.println( config.get("Test Case") + " => Failed"  );
-			System.out.println(e.getMessage());
-		}*/
-    	
-
+    public void getCollection_account(Map<String, String> config) throws JSONException {	
+    	try {
+        	Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
+        	System.out.println( "Passed => | " + config.get("Test Case ID") + " | " + config.get("Test Case Description") );
+        } catch (AssertionError e) {
+        	System.out.println( "Failed => | " + config.get("Test Case ID") + " | " + config.get("Test Case Description") );
+        	System.out.println(e.getMessage());
+        }
     }
     
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
-    public void readAccount(Map<String, String> config) throws JSONException {
-    	
-    	Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
-    	
-    	/*try {
-    		Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
-    		System.out.println( config.get("Test Case") + " => Passed"  );
-		} catch (AssertionError e) {
-			System.out.println( config.get("Test Case") + " => Failed"  );
-		}*/
-    	
+    public void get_account(Map<String, String> config) throws JSONException {    	
+    	try {
+        	Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
+        	System.out.println( "Passed => | " + config.get("Test Case ID") + " | " + config.get("Test Case Description") );
+        } catch (AssertionError e) {
+        	System.out.println( "Failed => | " + config.get("Test Case ID") + " | " + config.get("Test Case Description") );
+        	System.out.println(e.getMessage());
+        }
     }
-
+/*
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
     public void insertAccounts(Map<String, String> config) throws JSONException {
         Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
@@ -55,11 +47,11 @@ public class AccountServiceTest {
         Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
     }
     
-    /*@Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
+    @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
     public void insertAccounts(Map<String, String> config) throws JSONException {
         String insertParam = "{\"Header\":{},\"Params\":{\"Accounts\":[{\"Name\":\"test"+(new Date().getTime())+"\",\"AccountTypeID\":1}]}}";
         Util.callApi(config.get("URL"), insertParam, config.get("SchemaPath"), null);
-    }*/
+    }
 
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
     public void updateAccounts(Map<String, String> config) throws JSONException {
@@ -99,5 +91,5 @@ public class AccountServiceTest {
     @Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
     public void deactivateAccountContactAffiliations(Map<String, String> config) throws JSONException {
         Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
-    }
+    }*/
 }
--- CorporateAPIAutomation/src/test/java/api/corporate/ReferenceServiceTest.java
+++ CorporateAPIAutomation/src/test/java/api/corporate/ReferenceServiceTest.java
@@ -10,13 +10,25 @@ import java.util.Map;
 public class ReferenceServiceTest {
     
 	@Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
-    public void getCollection_reference(Map<String, String> config) throws JSONException {
-        Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
+    public void getCollection_reference(Map<String, String> config) throws JSONException { 
+        try {
+        	Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
+        	System.out.println( "Passed => | " + config.get("Test Case ID") + " | " + config.get("Test Case Description") );
+        } catch (AssertionError e) {
+        	System.out.println( "Failed => | " + config.get("Test Case ID") + " | " + config.get("Test Case Description") );
+        	System.out.println(e.getMessage());
+        }
     }
 	
 	@Test(dataProvider = "getAPIConfig", dataProviderClass = ExcelFileReaderConfig.class)
     public void get_reference(Map<String, String> config) throws JSONException {
-        Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
+		try {
+        	Util.callApi(config.get("URL"), config.get("Param"), config.get("SchemaPath"), null);
+        	System.out.println( "Passed => | " + config.get("Test Case ID") + " | " + config.get("Test Case Description") );
+        } catch (AssertionError e) {
+        	System.out.println( "Failed => | " + config.get("Test Case ID") + " | " + config.get("Test Case Description") );
+        	System.out.println(e.getMessage());
+        }
     }
 	
 /*	

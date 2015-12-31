3e917eaea9b9672d8e76ded8a151cd3c58bcc662
Hisham <hishamdiu@gmail.com>
2015-12-31 19:00:27 +0600

    Adding PostCollection for Address...

--- CorporateAPIAutomation/src/test/java/utilities/AppConstant.java
+++ CorporateAPIAutomation/src/test/java/utilities/AppConstant.java
@@ -6,7 +6,7 @@ import static com.jayway.restassured.RestAssured.*;
 
 public class AppConstant {
 //  public static final String BASE_URL = "http://localhost:8080";
-//	public static final String BASE_URL = "https://corporate.dev.d0003.campusops.net";
-	public static final String BASE_URL = "https://pt6joc38v8.execute-api.us-east-1.amazonaws.com";
+	public static final String BASE_URL = "https://corporate.dev.d0003.campusops.net";
+//	public static final String BASE_URL = "https://pt6joc38v8.execute-api.us-east-1.amazonaws.com";
 		
 }
